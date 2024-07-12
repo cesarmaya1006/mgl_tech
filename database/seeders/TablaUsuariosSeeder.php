0d950ea3b6f1021ea5658dcd2aee3178028deb02
cesarmaya1006 <cesarmaya1006@gmail.com>
2024-07-12 15:19:03 -0500

    mayo 2

--- database/seeders/TablaUsuariosSeeder.php
+++ database/seeders/TablaUsuariosSeeder.php
@@ -70,9 +70,9 @@ class TablaUsuariosSeeder extends Seeder
             'lider' => 1
         ]);
 
-        DB::table('tranv_empresas')->insert(['empleado_id' => 3, 'empresa_id' => 3, 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),]);
-        DB::table('tranv_empresas')->insert(['empleado_id' => 3, 'empresa_id' => 4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),]);
-        DB::table('tranv_empresas')->insert(['empleado_id' => 3, 'empresa_id' => 5, 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),]);
+        DB::table('tranv_empresas')->insert(['empleado_id' => 4, 'empresa_id' => 3, 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),]);
+        DB::table('tranv_empresas')->insert(['empleado_id' => 4, 'empresa_id' => 4, 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),]);
+        DB::table('tranv_empresas')->insert(['empleado_id' => 4, 'empresa_id' => 5, 'created_at' => Carbon::now()->format('Y-m-d H:i:s'),]);
         // -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
         $empleados = [
             ['usuario' => 'natalia.garzon', 'telefono' => '1073516926','cargo' => 1,'direccion' => 'Cond Mirasol Casa A1','lider' => 1],
